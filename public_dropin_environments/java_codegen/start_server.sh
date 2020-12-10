2db68fffb0a11bfda25434f8a3d2006ffec36eab
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-12-09 19:28:22 -0800

    tmp - comment out tests

--- public_dropin_environments/java_codegen/start_server.sh
+++ public_dropin_environments/java_codegen/start_server.sh
@@ -2,7 +2,7 @@
 cd /opt/code/ || exit 1
 export PYTHONPATH=$PYTHONPATH:/opt/code
 
-CMD="drum server -cd . --target-type ${TARGET_TYPE} --address 0.0.0.0:8080"
+CMD="drum server -cd . --address 0.0.0.0:8080"
 
 # Uncomment the following line to switch from Flask to uwsgi server
 # WITH_UWSGI="1"
@@ -12,15 +12,15 @@ else
   CMD="${CMD} --with-error-server"
 fi
 
-if [ -n "${POSITIVE_CLASS_LABEL}" ]; then
-    CMD="${CMD} --positive-class-label ${POSITIVE_CLASS_LABEL}"
-fi
-if [ -n "${NEGATIVE_CLASS_LABEL}" ]; then
-    CMD="${CMD} --negative-class-label ${NEGATIVE_CLASS_LABEL}"
-fi
-if [ -n "${CLASS_LABELS_FILE}" ]; then
-    CMD="${CMD} --class-labels-file ${CLASS_LABELS_FILE}"
-fi
+# if [ -n "${POSITIVE_CLASS_LABEL}" ]; then
+#     CMD="${CMD} --positive-class-label ${POSITIVE_CLASS_LABEL}"
+# fi
+# if [ -n "${NEGATIVE_CLASS_LABEL}" ]; then
+#     CMD="${CMD} --negative-class-label ${NEGATIVE_CLASS_LABEL}"
+# fi
+# if [ -n "${CLASS_LABELS_FILE}" ]; then
+#     CMD="${CMD} --class-labels-file ${CLASS_LABELS_FILE}"
+# fi
 
 echo "${CMD}"
 exec ${CMD}
